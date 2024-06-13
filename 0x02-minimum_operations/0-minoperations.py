6949790cf28f5769993c4a9d486e76b1cfaa4455
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-06-14 02:36:42 +0300

    “upload”

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,15 @@ Minimum Operations Coding Problem
 
 
 def minOperations(n):
-    '''Calculate the fewest number of operations 
+    '''Calculate the fewest number of operations
     needed to result in exactly n 'H' characters in a text file.
     Parameters:
     n (int): The target number of 'H' characters.
     Returns:
     int: The minimum number of operations
     '''
-    # If n is less than or equal to 1, it's impossible to achieve the target, return 0
+    # If n is less than or equal to 1 =>>
+    # it's impossible to achieve the target, return 0
     if n <= 1:
         return 0
     operations = 0
@@ -23,14 +24,14 @@ def minOperations(n):
     # Check for factors
     while (factor**2 <= n):
         if n % factor == 0:
-          # Add the factor to the operations count
+            # Add the factor to the operations count
             operations += factor
-            # Update n by dividing it by the current factor
+        # Update n by dividing it by the current factor
             n //= factor
         else:
             factor += 1
 
-    # If n is greater than 1, it means n is a prime number greater than its square root.
+    # If n>1, it means n is a prime number greater than its square root.
     if n > 1:
         operations += n
     # Return the total number of operations
